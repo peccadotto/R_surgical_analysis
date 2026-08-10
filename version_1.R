c70678e0104032728935612b225d3386f6ff5c1c
Peccadotto <andrea.peccati@gmail.com>
2026-08-11 00:08:42 +0200

    Final commit for version-1

--- version_1.R
+++ version_1.R
@@ -33,13 +33,13 @@
   
 # 3. Import data from .csv files
   files <- list.files(
-    path       = "data/ver_1",
+    path       = "data",
     pattern    = "surgical_data_\\d{4}\\.csv",
     full.names = TRUE
   )
   
   icd9proc <- read.csv(
-    "data/ver_1/icd9_procedures.csv",
+    "data/icd9_procedures.csv",
     header     = TRUE,
     sep        = ";",
     colClasses = "character"
@@ -119,7 +119,7 @@
     surgeries_total,
     surgeries_stats_wide,
     procedures_stats_wide,
-    file = "data/ver_1/export.RData"
+    file = "data/export.RData"
   )
   
 # -----------------------------------------------------------------------------
